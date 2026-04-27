ba8391fcd558bb79ab46e326ff8ac0a742a6ca48
Vojtěch Greger <107056257+wbtag@users.noreply.github.com>
2026-04-27 22:51:03 +0200

    Revert "Feature/dockerify"

--- vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    setupFiles: ['./tests/setup.js'],
-    testTimeout: 300000,
-    hookTimeout: 300000,
-  },
-});
